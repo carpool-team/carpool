a9deab66985fc066503f6bf6ad30986f3fb5c879
nlitkowski <norbert.litkowski@gmail.com>
2020-04-22 21:03:50 +0200

    Add android icons, fix shadows

--- src/Mobile/carpool/src/components/common/UpView.js
+++ src/Mobile/carpool/src/components/common/UpView.js
@@ -37,6 +37,7 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.2,
     shadowRadius: 8,
+    elevation: 5,
   },
   container: {
     flex: 1,
@@ -48,6 +49,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 1,
     shadowRadius: 6,
     backgroundColor: colors.background,
+    elevation: 5,
   },
 });
 
