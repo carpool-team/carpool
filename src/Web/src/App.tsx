6d7506af53ec2153596cb81f285b3e6b34450342
Maciej <masobkowiak@gmail.com>
2020-04-25 17:47:35 +0200

    CARP-52 Working on NavBar

--- src/Web/src/App.tsx
+++ src/Web/src/App.tsx
@@ -2,22 +2,23 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Switch } from "react-router";
 import { BreadcrumbsProvider } from "react-breadcrumbs-dynamic";
-import { Hello } from "./components/Hello/Hello";
+//import { Hello } from "./components/Hello/Hello";
+import { NavBar } from "./components/NavBar/NavBar";
 
 export default class App extends Component {
-	render() {
-		return (
-			<React.Fragment>
-				<Router>
-					<BreadcrumbsProvider>
-						<Switch>
-							<Route path={"/"}>
-								<Hello message={"Testing TS with React and WDS"}></Hello>
-							</Route>
-						</Switch>
-					</BreadcrumbsProvider>
-				</Router>
-			</React.Fragment>
-		);
-	}
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+          <BreadcrumbsProvider>
+            <Switch>
+              <Route path={"/"}>
+                <NavBar />
+              </Route>
+            </Switch>
+          </BreadcrumbsProvider>
+        </Router>
+      </React.Fragment>
+    );
+  }
 }
---
+++ src/Web/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./NavBar.scss";
+
+export const NavBar = () => {
+  return (
+    <>
+      <div id="container" className="navBarContainer">
+        <div className="logoContainer">
+          <img
+            src={require("../../assets/logo_small.png")}
+            alt="logo"
+            className="logo"
+          />
+        </div>
+        <nav className="navPlain">
+          <a className="navButtonPlain " href="/">
+            Pasażer
+          </a>
+          <a className="navButtonPlain " href="/">
+            Kierowca
+          </a>
+          <a className="navButtonPlain " href="/">
+            Firma
+          </a>
+        </nav>
+        <nav className="navAccount">
+          <button className="navButtonLogin">Zaloguj się</button>
+          <button className="navButtonRegister">Zarejestruj się</button>
+        </nav>
+      </div>
+    </>
+  );
+};
