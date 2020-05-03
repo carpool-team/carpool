6d8942efd2505dbb9e779bf8ff719f3de0de73d8
JKobrynski <juliankobrynski@gmail.com>
2020-05-03 15:10:19 +0200

    CARP-66 Point markers with onPress event and camera adjustments

---
+++ src/Mobile/carpool/src/examples/points.js
@@ -0,0 +1,42 @@
+export const examplePassengerPoints = [
+  {
+    id: Math.random(),
+    coordinates: [16.87619606957975, 52.44776959009451],
+    timeLeft: 35,
+  },
+  {
+    id: Math.random(),
+    coordinates: [16.872724766534958, 52.44373969820646],
+    timeLeft: 50,
+  },
+  {
+    id: Math.random(),
+    coordinates: [16.88208390230244, 52.437882807097054],
+    timeLeft: 19,
+  },
+  {
+    id: Math.random(),
+    coordinates: [16.86535340163644, 52.45314149371475],
+    timeLeft: 42,
+  },
+  {
+    id: Math.random(),
+    coordinates: [16.867204187861404, 52.44168865407403],
+    timeLeft: 90,
+  },
+  {
+    id: Math.random(),
+    coordinates: [16.890569263723165, 52.44689814009672],
+    timeLeft: 120,
+  },
+  {
+    id: Math.random(),
+    coordinates: [16.87987166828279, 52.45931604833598],
+    timeLeft: 7,
+  },
+  {
+    id: Math.random(),
+    coordinates: [16.854908387924553, 52.45528603351477],
+    timeLeft: 180,
+  },
+];
--- src/Mobile/carpool/src/screens/Home.js
+++ src/Mobile/carpool/src/screens/Home.js
@@ -1,23 +1,47 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import MapboxGL from '@react-native-mapbox-gl/maps';
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView, View, TouchableOpacity} from 'react-native';
 import colors from '../styles/colors';
 import HamburgerMenu from '../components/navigation/HamburgerMenu';
 import AccountSwitch from '../components/navigation/AccountSwitch';
+import Marker from '../components/common/Marker';
+import {vw} from '../utils/constants';
+import {examplePassengerPoints} from '../examples/points';
+
+const getColor = time => {
+  if (time < 20) {
+    return colors.red;
+  } else {
+    if (time < 45) {
+      return colors.orange;
+    } else {
+      if (time < 90) {
+        return colors.yellow;
+      } else {
+        return colors.green;
+      }
+    }
+  }
+};
 
 const Home = () => {
-  const [coordinates, setCoordinates] = useState([
-    [16.87939183014879, 52.445182084892735],
-  ]);
+  const [coordinates, setCoordinates] = useState([]);
+  const [center, setCenter] = useState([]);
 
   const _onLocateUser = e => {
     if (e) {
       const {
         coords: {latitude, longitude},
       } = e;
-      setCoordinates([[longitude, latitude]]);
+      setCoordinates([longitude, latitude]);
     }
   };
+
+  useEffect(() => {
+    if (!center.length) {
+      setCenter(coordinates);
+    }
+  }, [coordinates]);
 
   return (
     <View style={{flex: 1}}>
@@ -27,16 +51,33 @@ const Home = () => {
           <AccountSwitch />
           <MapboxGL.MapView
             style={{flex: 1}}
-            onPress={event => console.log(event)}
             styleURL="mapbox://styles/jkobrynski/ck9632hsy2m4q1invvx1jjvo9/draft">
             <MapboxGL.Camera
-              zoomLevel={16}
+              zoomLevel={14}
               animationMode="flyTo"
-              animationDuration={0}
-              followUserLocation
-              followUserMode={'normal'}
+              animationDuration={500}
+              //followUserLocation={!center.length}
+              //followUserMode={'normal'}
+              centerCoordinate={center}
             />
             <MapboxGL.UserLocation visible onUpdate={_onLocateUser} />
+            {examplePassengerPoints.map(point => (
+              <MapboxGL.PointAnnotation
+                key={point.id}
+                id="selected"
+                coordinate={point.coordinates}
+                onSelected={e => setCenter(point.coordinates)}
+                onDeselected={() => setCenter(coordinates)}>
+                <Marker
+                  color={getColor(point.timeLeft)}
+                  size={6 * vw}
+                  style={{
+                    marginTop: -6 * vw,
+                    padding: 2 * vw,
+                  }}
+                />
+              </MapboxGL.PointAnnotation>
+            ))}
           </MapboxGL.MapView>
         </View>
       </SafeAreaView>
