9d80219016108cc81424387e05623b5a0f63f728
JKobrynski <juliankobrynski@gmail.com>
2020-05-04 15:23:21 +0200

    Android location fix (permissions)

--- src/Mobile/carpool/src/screens/Home.js
+++ src/Mobile/carpool/src/screens/Home.js
@@ -1,14 +1,44 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import MapboxGL from '@react-native-mapbox-gl/maps';
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView, View, PermissionsAndroid, Platform} from 'react-native';
 import colors from '../styles/colors';
 import HamburgerMenu from '../components/navigation/HamburgerMenu';
 import AccountSwitch from '../components/navigation/AccountSwitch';
+
+const requestLocationPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Carpool',
+        message:
+          'Carpool needs access to your location ' +
+          'so you can use the app properly.',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log('You can use the location');
+    } else {
+      console.log('Location permission denied');
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+};
 
 const Home = () => {
   const [coordinates, setCoordinates] = useState([
     [16.87939183014879, 52.445182084892735],
   ]);
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      requestLocationPermission();
+    }
+  }, []);
 
   const _onLocateUser = e => {
     if (e) {
