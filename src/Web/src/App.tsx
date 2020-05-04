54b9cb089055bcf1901e094791bc15061ce1024f
Maciej <masobkowiak@gmail.com>
2020-05-04 07:35:26 +0200

    CARP-34 Added loading screen

--- src/Web/src/App.tsx
+++ src/Web/src/App.tsx
@@ -3,19 +3,20 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Switch } from "react-router";
 import { BreadcrumbsProvider } from "react-breadcrumbs-dynamic";
 import Layout from "./components/layout/Layout";
+import LoadingScreen from "./components/LoadingScreen/LoadingScreen";
 
 export default class App extends Component {
-	render() {
-		return (
-			<React.Fragment>
-				<Router>
-					<BreadcrumbsProvider>
-						<Switch>
-							<Route path={"/"} component={Layout} />
-						</Switch>
-					</BreadcrumbsProvider>
-				</Router>
-			</React.Fragment>
-		);
-	}
+  render() {
+    return (
+      <React.Fragment>
+        <Router>
+          <BreadcrumbsProvider>
+            <Switch>
+              <Route path={"/"} component={LoadingScreen} />
+            </Switch>
+          </BreadcrumbsProvider>
+        </Router>
+      </React.Fragment>
+    );
+  }
 }
