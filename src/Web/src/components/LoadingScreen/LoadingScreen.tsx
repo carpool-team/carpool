edee546a0ad5ea6e7cd059dce1f4799fcbc460c0
Norbert Litkowski <norbert.litkowski@gmail.com>
2020-05-06 22:10:04 +0200

    Fix layout sizing

--- src/Web/src/components/LoadingScreen/LoadingScreen.tsx
+++ src/Web/src/components/LoadingScreen/LoadingScreen.tsx
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
+import { withTranslation } from "react-i18next";
+import { IReactI18nProps } from "components/system/resources/IReactI18nProps";
+
 import "./LoadingScreen.scss";
 
-export default class LoadingScreen extends Component {
+class LoadingScreen extends Component<IReactI18nProps> {
+	private cssClasses = {
+		container: "loadingScreen",
+		map: "loadingScreen__map",
+		quote: "loadingScreen__quote",
+	};
+
+	private resources = {
+		quote: "loadingScreen.quote",
+	};
+
 	render: () => JSX.Element = () => {
+		const { t } = this.props;
 		return (
-			<div>
-				<div className="map">
-					<div className="quote">Share a ride</div>
+			<div className={this.cssClasses.container}>
+				<div className={this.cssClasses.map}>
+					<div className={this.cssClasses.quote}>
+						{t(this.resources.quote)}
+					</div>
 				</div>
 			</div>
 		);
 	}
 }
+
+export default withTranslation()(LoadingScreen);
