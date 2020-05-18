22aff14914aecb6d490adbdc4b1dcb734895727c
Maciej <masobkowiak@gmail.com>
2020-05-18 15:14:01 +0200

    CARP-113 Working on groups baner

--- src/Web/src/components/LoadingScreen/LoadingScreen.tsx
+++ src/Web/src/components/LoadingScreen/LoadingScreen.tsx
@@ -1,19 +1,29 @@
 import React, { Component } from "react";
 import { withTranslation } from "react-i18next";
 import { IReactI18nProps } from "components/system/resources/IReactI18nProps";
+import Button from "../ui/Button/Button";
+import { ButtonType } from "../ui/Button/enums/ButtonType";
 
 import "./LoadingScreen.scss";
+
+import groupImg from "assets_path/img/groups_baner.png";
+
 
 class LoadingScreen extends Component<IReactI18nProps> {
 	private cssClasses = {
 		container: "loadingScreen",
 		map: "loadingScreen__map",
 		quote: "loadingScreen__quote",
+		groups: "loadingScreen__groups",
+		groups_baner: "loadingScreen__groups--baner",
+		groups_button: "loadingScreen__groups--button"
 	};
 
 	private resources = {
 		quote: "loadingScreen.quote",
+		join: "loadingScreen.join"
 	};
+	
 
 	render: () => JSX.Element = () => {
 		const { t } = this.props;
@@ -22,6 +32,19 @@ class LoadingScreen extends Component<IReactI18nProps> {
 				<div className={this.cssClasses.map}>
 					<div className={this.cssClasses.quote}>
 						{t(this.resources.quote)}
+					</div>
+				</div>
+				<div className={this.cssClasses.groups}>
+					<div className={this.cssClasses.groups_button}>
+						<Button
+							onClick={()=>{}}
+							type={ButtonType.Info}
+			    		>
+							{t(this.resources.join)}
+						</Button>
+					</div>
+					<div className={this.cssClasses.groups_baner}>
+						<img src={groupImg}></img>
 					</div>
 				</div>
 			</div>
