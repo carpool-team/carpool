92e6a0e95b5edc096dbce1e0a413d365be1c85e2
Norbert Litkowski <norbert.litkowski@gmail.com>
2020-11-11 21:13:48 +0100

    Post-merge fix

--- src/Web/src/components/groups/components/manageScreen/components/GroupsList.tsx
+++ src/Web/src/components/groups/components/manageScreen/components/GroupsList.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { IGroup } from "../../../interfaces/IGroup";
 
-import exampleGroups from "../../../../../examples/exampleGroups"
-
 interface IGroupsListProps {
 	getGroupsCallback: () => IGroup[];
 	setGroupChecked: (id: string, checked: boolean) => void;
@@ -10,28 +8,26 @@ interface IGroupsListProps {
 
 const GroupsList = (props: IGroupsListProps) => {
 	const listCssClass: string = "groupsManagementList";
-	const labelCssClass:string = "groupsManagementList--label"
-	const buttonCssClass:string = "groupsManagementList--button"
-	const pinCssClass:string = "groupsManagementList--pin"
+	const labelCssClass: string = "groupsManagementList--label";
+	const buttonCssClass: string = "groupsManagementList--button";
+	const pinCssClass: string = "groupsManagementList--pin";
 
-	{/* TODO zmienić z mockupów na api jak zacznie działać */}
-	{/* const groups:IGroup[] = props.getGroupsCallback(); */}
-	const groups:IGroup[] = exampleGroups
+	const groups: IGroup[] = props.getGroupsCallback();
 
-	let colorList:string[] = ["#C39BD3", "#7FB3D5","#48C9B0","#F9E79F"]
-	let colorIndex:number = 0;
+	let colorList: string[] = ["#C39BD3", "#7FB3D5", "#48C9B0", "#F9E79F"];
+	let colorIndex: number = 0;
 
 	return (
 		<ul className={listCssClass}>
 			{groups.map((group) => {
 				++colorIndex;
 				const color = {
-					color: colorList[colorIndex%colorList.length]
+					color: colorList[colorIndex % colorList.length]
 				};
 				return (
 					<li key={group.name}>
-						<button className={buttonCssClass}>	
-						<div className={pinCssClass} style={color}>	</div>			
+						<button className={buttonCssClass}>
+							<div className={pinCssClass} style={color}>	</div>
 							<div className={labelCssClass}>
 								{group.name}
 							</div>
