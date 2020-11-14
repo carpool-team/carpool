8cf05f0c189bf3818ae7f73d6736af39d5b63cb1
Julian Kobryński <juliankobrynski@gmail.com>
2020-11-14 11:31:02 +0100

    CARP-287 Sign up screen placed in navigator

---
+++ src/Mobile/carpool/src/navigation/MainStackNavigator/AuthStack/index.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import SignUp from '../../../screens/AuthStack/SignUp';
+
+const Stack = createStackNavigator();
+
+const AuthStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="SignUp"
+      component={SignUp}
+      options={{
+        headerShown: false,
+      }}
+    />
+  </Stack.Navigator>
+);
+
+export default AuthStack;
--- src/Mobile/carpool/src/navigation/MainStackNavigator/index.js
+++ src/Mobile/carpool/src/navigation/MainStackNavigator/index.js
@@ -2,16 +2,24 @@ import * as React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import OnBoarding from '../../screens/OnBoarding';
 import DrawerNavigator from './DrawerNavigator';
+import AuthStack from './AuthStack';
 
 const Stack = createStackNavigator();
 
 const MainStackNavigator = () => (
   <Stack.Navigator>
-    <Stack.Screen
+    {/* <Stack.Screen
       name="OnBoarding"
       component={OnBoarding}
       options={{
         header: () => null,
+      }}
+    /> */}
+    <Stack.Screen
+      name="AuthStack"
+      component={AuthStack}
+      options={{
+        headerShown: false,
       }}
     />
     <Stack.Screen
