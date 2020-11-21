9abe71f361e9aa220de3775740382f395029c415
Julian Kobryński <juliankobrynski@gmail.com>
2020-11-21 14:43:23 +0100

    CARP-353 Matching rides with days of the week

---
+++ src/Mobile/carpool/src/components/Driver/WeekRidesList/utils.js
@@ -0,0 +1,28 @@
+import moment from 'moment';
+
+export const matchRides = (rides, day) => {
+  const current = moment(day);
+
+  const filtered = rides.filter(ride => {
+    const date = moment(ride.date);
+
+    if (date.isSame(current, 'day')) {
+      return ride;
+    }
+  });
+
+  if (filtered.length) {
+    return [...filtered];
+  }
+  return null;
+};
+
+export const getWeekday = date => {
+  return moment(date)
+    .format('ddd D')
+    .toLocaleUpperCase();
+};
+
+export const isToday = day => {
+  return moment().isSame(moment(day), 'day');
+};
