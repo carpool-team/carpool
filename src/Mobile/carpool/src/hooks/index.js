e84792cd1257d989d500a890e876cdf901e7ba72
Julian Kobryński <juliankobrynski@gmail.com>
2020-11-22 15:40:43 +0100

    CARP-369 useActiveAccount hook

--- src/Mobile/carpool/src/hooks/index.js
+++ src/Mobile/carpool/src/hooks/index.js
@@ -1,5 +1,13 @@
 import useForwardGeocoding from './useForwardGeocoding';
 import useRequest, {METHODS, ENDPOINTS} from './useRequest';
 import useGetDirections from './useGetDirections';
+import useActiveAccount from './useActiveAccount';
 
-export {useForwardGeocoding, useRequest, useGetDirections, METHODS, ENDPOINTS};
+export {
+  useForwardGeocoding,
+  useRequest,
+  useGetDirections,
+  METHODS,
+  ENDPOINTS,
+  useActiveAccount,
+};
