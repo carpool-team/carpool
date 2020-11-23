0878ed6337878a2853d3787de165483d496ff04f
Michał Dulski <dulski.michal@outlook.com>
2020-11-23 10:21:27 +0100

    [CARP-285] [CARP-293] chore(auth): Fixing keys

--- src/API/AuthService/AuthServer/Program.cs
+++ src/API/AuthService/AuthServer/Program.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Azure.Identity;
 
 namespace AuthServer
 {
@@ -19,13 +18,6 @@ namespace AuthServer
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-.ConfigureAppConfiguration((context, config) =>
-{
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-config.AddAzureKeyVault(
-keyVaultEndpoint,
-new DefaultAzureCredential());
-})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
