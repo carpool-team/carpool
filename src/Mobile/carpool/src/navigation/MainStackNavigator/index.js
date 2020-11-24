2cf23ad1d693615618fdb78949cbe2670dc303ea
Julian Kobryński <juliankobrynski@gmail.com>
2020-11-24 19:09:02 +0100

    CARP-279 Conditional rendering in main navigator

--- src/Mobile/carpool/src/navigation/MainStackNavigator/index.js
+++ src/Mobile/carpool/src/navigation/MainStackNavigator/index.js
@@ -1,35 +1,61 @@
-import * as React from 'react';
+import React, {useState, useEffect} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import OnBoarding from '../../screens/OnBoarding';
 import DrawerNavigator from './DrawerNavigator';
 import AuthStack from './AuthStack';
+import {useDispatch, useSelector} from 'react-redux';
+import {readData, STORAGE_KEYS} from '../../storage';
+import * as actions from '../../store/actions/auth';
 
 const Stack = createStackNavigator();
 
-const MainStackNavigator = () => (
-  <Stack.Navigator>
-    {/* <Stack.Screen
-      name="OnBoarding"
-      component={OnBoarding}
-      options={{
-        header: () => null,
-      }}
-    /> */}
-    <Stack.Screen
-      name="AuthStack"
-      component={AuthStack}
-      options={{
-        headerShown: false,
-      }}
-    />
-    <Stack.Screen
-      name="DrawerNavigator"
-      component={DrawerNavigator}
-      options={{
-        header: () => null,
-      }}
-    />
-  </Stack.Navigator>
-);
+const MainStackNavigator = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const dispatch = useDispatch();
+  const tokens = useSelector(state => state.authReducer.tokens);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const token = await readData(STORAGE_KEYS.token);
+        const refreshToken = await readData(STORAGE_KEYS.refreshToken);
+
+        if (token && refreshToken) {
+          dispatch(actions.getTokenSuccess({token, refreshToken}));
+        }
+      } finally {
+        setIsLoaded(true);
+      }
+    })();
+  });
+
+  if (!isLoaded) {
+    return <OnBoarding />;
+  }
+
+  return (
+    <Stack.Navigator>
+      {!tokens.data && (
+        <Stack.Screen
+          name="AuthStack"
+          component={AuthStack}
+          options={{
+            headerShown: false,
+          }}
+        />
+      )}
+      {tokens.data && (
+        <Stack.Screen
+          name="DrawerNavigator"
+          component={DrawerNavigator}
+          options={{
+            header: () => null,
+          }}
+        />
+      )}
+    </Stack.Navigator>
+  );
+};
 
 export default MainStackNavigator;
