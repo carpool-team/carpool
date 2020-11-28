11e7dd9e8c2ee729c8d7da94f9a24ecbd8a48454
Maciej Sobkowiak <masobkowiak@gmail.com>
2020-11-29 00:27:42 +0100

    CARP-420 Added geoJson usage

--- src/Web/src/components/map/MapBoxRides.tsx
+++ src/Web/src/components/map/MapBoxRides.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
 import { CSSProperties } from "react";
-import ReactMapboxGl, { Layer, Feature, Popup, Marker } from "react-mapbox-gl";
-import { IGroup } from "../groups/interfaces/IGroup";
+import ReactMapboxGl, { Layer, Feature, Popup, Marker, GeoJSONLayer} from "react-mapbox-gl";
 import mapConfig from "./mapConfig";
 import { colorList } from "../../scss/colorList";
 import produce from "immer";
 import { FitBoundsOptions } from "react-mapbox-gl/lib/map";
 import { IRide } from "components/groups/interfaces/IRide";
+import mapboxDirections from "@mapbox/mapbox-sdk/services/directions";
 
 const Mapbox = ReactMapboxGl({
 	// TODO jak bedą grupy z lokacją to zmienić na prawidłowy -> około 8
@@ -15,11 +15,14 @@ const Mapbox = ReactMapboxGl({
 	accessToken: mapConfig.mapboxKey
 });
 
+const directionsClient = mapboxDirections({accessToken: mapConfig.mapboxKey});
+
 export interface IMapState {
 	fitBounds?: [[number, number], [number, number]];
 	center?: [number, number];
 	zoom?: [number];
 	ride: IRide;
+	route: any;
 }
 
 const flyToOptions = {
@@ -28,7 +31,8 @@ const flyToOptions = {
 
 const defaults = {
 	zoom: [11] as [number],
-	center: [-0.109970527, 51.52916347] as [number, number],
+	center: [-77.01239, 38.91275] as [number, number],
+	route: null
 };
 
 export interface IMapProps {
@@ -43,25 +47,50 @@ export default class MapBoxGroups extends React.Component<IMapProps, IMapState> 
 		this.state = {
 			fitBounds: undefined,
 			ride: this.props.ride,
+			route: undefined,
 			...defaults,
 		};
+	}
+	private onFindRoute = async (ride: IRide) => {
+		try {
+			const response = await directionsClient
+				.getDirections({
+					profile: "driving-traffic",
+					waypoints: [
+						{
+							coordinates: [ride.startingLocation.latitude, ride.startingLocation.longitude],
+						},
+						{
+							coordinates: [ride.destination.latitude, ride.destination.longitude]
+						},
+					],
+					overview: "full",
+					geometries: "geojson",
+					// alternatives: true,
+				})
+				.send();
+				this.setState(produce((draft: any) => {
+					draft.route = response.body.routes[0].geometry.coordinates;
+				}));
+
+			} catch (err) {
+				console.log(err);
+			} finally {
+		}
 	}
 
 	componentDidUpdate() {
 		if (this.state.ride !== this.props.ride) {
+			this.onFindRoute(this.props.ride);
+			this.getBounds(this.props.ride);
 			this.setState(produce((draft: IMapState) => {
 				draft.ride = this.props.ride;
-				// TODO  Obliczać prawidłowo zoom
-				draft.zoom = [12];
-				draft.fitBounds = this.getBounds(this.props.ride);
-				draft.center = this.getCenter(draft.fitBounds);
 			}));
 		}
 	}
 
 	private getBounds = (ride: IRide) => {
 		const allCoords = [[ride.destination.latitude, ride.startingLocation.latitude], [ride.destination.longitude, ride.startingLocation.longitude]];
-		console.log(allCoords);
 		let bbox: [[number, number], [number, number]] = [[0, 0], [0, 0]];
 
 		if (allCoords[0].length !== 0 && allCoords[1].length !== 0) {
@@ -70,10 +99,11 @@ export default class MapBoxGroups extends React.Component<IMapProps, IMapState> 
 			bbox[0][1] = Math.min.apply(null, allCoords[1]);
 			bbox[1][1] = Math.max.apply(null, allCoords[1]);
 		}
-		console.log(bbox);
-
-		return bbox;
+		this.setState(produce((draft: any) => {
+			draft.fitBounds = bbox;
+		}));
 	}
+
 	private getCenter = (bbox: [[number, number], [number, number]]) => {
 		let cords: [number, number] = [0, 0];
 		cords[0] = (bbox[0][0] + bbox[1][0]) / 2;
@@ -87,14 +117,14 @@ export default class MapBoxGroups extends React.Component<IMapProps, IMapState> 
 	}
 
 	public render() {
-		const { fitBounds, center, zoom, ride } = this.state;
+		const { fitBounds, center, zoom, ride, route } = this.state;
 
 		const containerStyle: CSSProperties = {
 			height: "100%",
 		};
 
 		const boundsOptions: FitBoundsOptions = {
-			padding: 20
+			padding: 50
 		};
 
 		const popupStyle: CSSProperties = {
@@ -111,6 +141,26 @@ export default class MapBoxGroups extends React.Component<IMapProps, IMapState> 
 			fontSize: "40px",
 			color: "#ee5253"
 		};
+		const lineLayout = {
+			"line-cap": "round" ,
+			"line-join": "round"
+		};
+		const linePaint = {
+			"line-color": "#6b98d1",
+			"line-width": 10
+		};
+		const geojson = {
+			"type": "FeatureCollection",
+			"features": [
+				{
+					"type": "Feature",
+					"geometry": {
+						"type": "LineString",
+						"coordinates": route
+					}
+				}
+			]
+		};
 
 		return (
 			<Mapbox
@@ -123,9 +173,16 @@ export default class MapBoxGroups extends React.Component<IMapProps, IMapState> 
 				containerStyle={containerStyle}
 				flyToOptions={flyToOptions}
 			>
+				{route !== null &&
+					<GeoJSONLayer
+						data={geojson}
+						linePaint={linePaint}
+						lineLayout={lineLayout}
+					/>
+				}
 				{ride !== null &&
 				<>
-				<Marker
+				{/* <Marker
 					style= {toMarkerStyle}
 					coordinates={[ride.destination.latitude, ride.destination.longitude]}
 					anchor="bottom"
@@ -138,7 +195,7 @@ export default class MapBoxGroups extends React.Component<IMapProps, IMapState> 
 					anchor="bottom"
 					>
 					<i className={"fa fa-map-marker"}></i>
-				</Marker>
+				</Marker> */}
 				<Popup coordinates={[ride.destination.latitude, ride.destination.longitude]}>
 					<div style={popupStyle}>
 						{"Lokalizacja końcowa."}
@@ -151,7 +208,6 @@ export default class MapBoxGroups extends React.Component<IMapProps, IMapState> 
 				</Popup>
 				</>
 				}
-			</Mapbox>
-		);
+			</Mapbox>		);
 	}
 }
