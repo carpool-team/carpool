537eee28ec0cad4abe74ec9b46ee7cd7e8b562e9
Julian Kobryński <juliankobrynski@gmail.com>
2020-11-29 14:56:41 +0100

    CARP-424 Mapbox key env variable

--- src/Mobile/carpool/src/maps/mapbox.js
+++ src/Mobile/carpool/src/maps/mapbox.js
@@ -1,8 +1,8 @@
 import mapboxClient from '@mapbox/mapbox-sdk';
 import mapboxGeocoding from '@mapbox/mapbox-sdk/services/geocoding';
 import mapboxDirections from '@mapbox/mapbox-sdk/services/directions';
-import config from '../../config';
+import {MAPBOX_KEY} from '@env';
 
-export const baseClient = mapboxClient({accessToken: config.mapboxKey});
+export const baseClient = mapboxClient({accessToken: MAPBOX_KEY});
 export const geocodingClient = mapboxGeocoding(baseClient);
 export const directionsClient = mapboxDirections(baseClient);
