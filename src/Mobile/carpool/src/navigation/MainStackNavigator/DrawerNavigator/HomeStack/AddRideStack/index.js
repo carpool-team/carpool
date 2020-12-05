e68b044c424c571a8c8c16b1b709e37dba3ddc66
Julian Kobryński <juliankobrynski@gmail.com>
2020-12-05 18:24:47 +0100

    CARP-429 Reseting context on unfocus

--- src/Mobile/carpool/src/navigation/MainStackNavigator/DrawerNavigator/HomeStack/AddRideStack/index.js
+++ src/Mobile/carpool/src/navigation/MainStackNavigator/DrawerNavigator/HomeStack/AddRideStack/index.js
@@ -4,40 +4,65 @@ import ChooseRoute from '../../../../../screens/HomeStack/AddRideStack/ChooseRou
 import PickTime from '../../../../../screens/HomeStack/AddRideStack/PickTime';
 import SetSeats from '../../../../../screens/HomeStack/AddRideStack/SetSeats';
 import SelectGroup from '../../../../../screens/HomeStack/AddRideStack/SelectGroup';
+import SelectDirection from '../../../../../screens/HomeStack/AddRideStack/SelectDirection';
+import {useIsFocused} from '@react-navigation/native';
+import {
+  AddRideContext,
+  AddRideContextActions,
+} from '../../../../../screens/HomeStack/AddRideStack/context';
+
+const {useContext, useEffect} = React;
 
 const Stack = createStackNavigator();
 
-const AddRideStack = props => (
-  <Stack.Navigator>
-    <Stack.Screen
-      name="SelectGroup"
-      component={SelectGroup}
-      options={{
-        headerShown: false,
-      }}
-    />
-    <Stack.Screen
-      name="ChooseRoute"
-      component={ChooseRoute}
-      options={{
-        headerShown: false,
-      }}
-    />
-    <Stack.Screen
-      name="PickTime"
-      component={PickTime}
-      options={{
-        headerShown: false,
-      }}
-    />
-    <Stack.Screen
-      name="SetSeats"
-      component={SetSeats}
-      options={{
-        headerShown: false,
-      }}
-    />
-  </Stack.Navigator>
-);
+const AddRideStack = props => {
+  const focused = useIsFocused();
+
+  const {dispatch} = useContext(AddRideContext);
+
+  useEffect(() => {
+    !focused && dispatch({type: AddRideContextActions.CLEAN_STATE});
+  }, [focused]);
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="SelectGroup"
+        component={SelectGroup}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="SelectDirection"
+        component={SelectDirection}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="ChooseRoute"
+        component={ChooseRoute}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="PickTime"
+        component={PickTime}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="SetSeats"
+        component={SetSeats}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
 
 export default AddRideStack;
