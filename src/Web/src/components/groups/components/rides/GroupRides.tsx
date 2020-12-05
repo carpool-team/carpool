7de5b48369d8ea22fe7759bd6e0713343208b7f3
Maciej Sobkowiak <masobkowiak@gmail.com>
2020-12-05 22:11:53 +0100

    CARP-423 Added swietch to groupRides View

--- src/Web/src/components/groups/components/rides/GroupRides.tsx
+++ src/Web/src/components/groups/components/rides/GroupRides.tsx
@@ -18,6 +18,9 @@ import { withRouter, RouteComponentProps } from "react-router-dom";
 import { withTranslation } from "react-i18next";
 import { IReactI18nProps } from "../../../system/resources/IReactI18nProps";
 import GroupsRouter from "../GroupsRouter";
+import Switch, { SwitchClassKey, SwitchProps } from "@material-ui/core/Switch";
+import { withStyles, Theme, createStyles } from "@material-ui/core/styles";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 interface IGroupRidesProps extends IGroupDetailedViewProps, RouteComponentProps, IReactI18nProps {
 
@@ -38,17 +41,53 @@ const GroupRides = (props: IGroupRidesProps) => {
 	};
 	const resources = {
 		add: "addBtn",
-		back: "prevBtn"
+		back: "prevBtn",
+		participant: "rides.participantOnly",
+		owner: "rides.ownerOnly"
 	};
 
 	const rides: IRide[] = ridesExample;
 	const [selectedRide, setSelectedRide] = useState(null);
+	const [userParticipant, setUserParticipant] = useState(false);
+	const [userOwner, setUserOwner] = useState(false);
 
 	const setRide = (ride: IRide) => {
 		if (ride !== null) {
 			setSelectedRide(ride);
 		}
 	};
+	const handleSwitchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		if (event.target.name === "participant") {
+			setUserParticipant(event.target.checked);
+			if (userOwner) {
+				setUserOwner(false);
+			}
+		}
+		if (event.target.name === "owner") {
+			if (userParticipant) {
+				setUserParticipant(false);
+			}
+			setUserOwner(event.target.checked);
+		}
+	};
+
+	const UserSwitch = withStyles({
+		switchBase: {
+			color: "#6b98d1",
+			"&$checked": {
+				color: "#6b98d1",
+			},
+			"&$checked + $track": {
+				backgroundColor: "#4a90e8",
+			},
+			"& + $track": {
+				backgroundColor: "#707070",
+			}
+		},
+		checked: {},
+		track: {},
+	})(Switch);
+
 	const { url } = props.match;
 	const { t } = props;
 
@@ -75,7 +114,14 @@ const GroupRides = (props: IGroupRidesProps) => {
 					</ButtonLink>
 				</div>
 				<div>
-
+					<FormControlLabel
+						control={<UserSwitch size="medium" checked={userParticipant} onChange={handleSwitchChange} name={"participant"}/>}
+						label={t(resources.participant)}
+					/>
+					<FormControlLabel
+						control={<UserSwitch size="medium" checked={userOwner} onChange={handleSwitchChange} name={"owner"}/>}
+						label={t(resources.owner)}
+					/>
 				</div>
 				<div className={cssClasses.leftOutline}></div>
 				<div className={cssClasses.leftList}>
