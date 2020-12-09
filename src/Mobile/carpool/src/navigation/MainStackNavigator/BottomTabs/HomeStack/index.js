47ccb063aed3238c99d017d83cc7d4723dac042b
Julian Kobryński <juliankobrynski@gmail.com>
2020-12-09 17:19:33 +0100

    CARP-502 New home screen in navigator

--- src/Mobile/carpool/src/navigation/MainStackNavigator/BottomTabs/HomeStack/index.js
+++ src/Mobile/carpool/src/navigation/MainStackNavigator/BottomTabs/HomeStack/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-import Home from '../../../../screens/HomeStack/Home';
+// import Home from '../../../../screens/HomeStack/Home';
+import {Home} from '../../../../screens/BottomTabs/HomeStack';
 
 const Stack = createStackNavigator();
 
