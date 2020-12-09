3aade054726f82c9ffe29bcef7757fce8c55d00c
Norbert Litkowski <norbert.litkowski@gmail.com>
2020-12-09 02:22:40 +0100

    CARP-492 Add getting rides from API

--- src/Web/src/components/rides/Rides.tsx
+++ src/Web/src/components/rides/Rides.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router";
 import ButtonSmall from "../ui/buttonSmall/ButtonSmall";
 import { ButtonSmallBackground } from "../ui/buttonSmall/enums/ButtonSmallBackground";
 import { ButtonSmallColor } from "../ui/buttonSmall/enums/ButtonSmallColor";
@@ -7,26 +6,41 @@ import { ButtonSmallIcon } from "../ui/buttonSmall/enums/ButtonSmallIcon";
 import ButtonLink from "../ui/buttonLink/ButtonLink";
 import { ButtonLinkBackground } from "../ui/buttonLink/enums/ButtonLinkBackground";
 import { ButtonLinkColor } from "../ui/buttonLink/enums/ButtonLinkColor";
-import {ButtonLinkStyle} from "../ui/buttonLink/enums/ButtonLinkStyle";
-import { mainRoutes } from "../layout/components/LayoutRouter";
-import ridesExample from "../../examples/exampleRides";
+import { ButtonLinkStyle } from "../ui/buttonLink/enums/ButtonLinkStyle";
 import { IRide } from "../../components/groups/interfaces/IRide";
 import MediaQuery from "react-responsive";
 import MapBoxRides from "../map/MapBoxRides";
 import { withRouter, RouteComponentProps } from "react-router-dom";
 import { withTranslation } from "react-i18next";
 import { IReactI18nProps } from "../system/resources/IReactI18nProps";
-import Switch, { SwitchClassKey, SwitchProps } from "@material-ui/core/Switch";
-import { withStyles, Theme, createStyles } from "@material-ui/core/styles";
+import Switch from "@material-ui/core/Switch";
+import { withStyles } from "@material-ui/core/styles";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import RidesOwner from "./components/RidesOwner";
 import RidesParticipant from "./components/RidesParticipant";
 import moment from "moment";
 import "./Rides.scss";
-import { random } from "lodash";
-import exampleRides from "../../examples/exampleRides";
+import { connect } from "react-redux";
+import { IGroupsState } from "../groups/store/State";
 
-interface IRidesProps extends RouteComponentProps, IReactI18nProps {
+interface IStatePropsType {
+	groups: IGroupsState;
+}
+
+interface IStateToProps {
+	ridesOwned: IRide[];
+	ridesParticipated: IRide[];
+
+}
+
+const mapStateToProps = (state: IStatePropsType): IStateToProps => ({
+	ridesOwned: state.groups.ridesOwned,
+	ridesParticipated: state.groups.ridesParticipated,
+});
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+
+interface IRidesProps extends RouteComponentProps, IReactI18nProps, StateProps {
 
 }
 
@@ -58,13 +72,9 @@ const Rides = (props: IRidesProps) => {
 		from: "fromId"
 	};
 
-	// Pobierać dane z serwera z odpowiedniego endpointu
-	const [ridesParticipant, setRidesParticipant] = useState(exampleRides);
-	const [ridesOwner, setRidesOwner] = useState(exampleRides);
-
 	const [selectedRide, setSelectedRide] = useState(null);
 	const [userOwner, setUserOwner] = useState(false);
-	const [switchCssClass, setSwitchCssClass] = useState({from: cssClasses.switchActive, to: null});
+	const [switchCssClass, setSwitchCssClass] = useState({ from: cssClasses.switchActive, to: null });
 
 	const setRide = (ride: IRide) => {
 		if (ride !== null) {
@@ -75,13 +85,13 @@ const Rides = (props: IRidesProps) => {
 		setUserOwner(event.target.checked);
 		setSelectedRide(null);
 		if (event.target.checked) {
-			setSwitchCssClass({from: null, to: cssClasses.switchActive});
+			setSwitchCssClass({ from: null, to: cssClasses.switchActive });
 		} else {
-			setSwitchCssClass({from: cssClasses.switchActive, to: null});
+			setSwitchCssClass({ from: cssClasses.switchActive, to: null });
 		}
 	};
 
-	const getWeek = offset => {
+	const getWeek = (offset: number) => {
 		const start = moment()
 			.add(offset, "weeks")
 			.startOf("isoWeek");
@@ -98,7 +108,7 @@ const Rides = (props: IRidesProps) => {
 
 		return week;
 	};
-	const getDates = offset => {
+	const getDates = (offset: number) => {
 		const week = getWeek(offset);
 		const range = `${moment(week[0]).format("DD.MM")} - ${moment(week[6]).format(
 			"DD.MM",
@@ -117,20 +127,19 @@ const Rides = (props: IRidesProps) => {
 	const [date, setDate] = useState(getDates(0));
 
 	const onNextDate = () => {
-		const newOffset = dateOffset + 1 ;
+		const newOffset = dateOffset + 1;
 		setDate(getDates(newOffset));
 		setDateOffset(newOffset);
 		setSelectedRide(null);
 	};
 	const onPrevDate = () => {
-		const newOffset = dateOffset - 1 ;
+		const newOffset = dateOffset - 1;
 		setDate(getDates(newOffset));
 		setDateOffset(newOffset);
 		setSelectedRide(null);
 	};
 
 	const matchRides = (rides: IRide[]) => {
-
 		const filtered = rides.filter(ride => {
 			const current = moment(ride.date);
 			if (current.isBetween(moment(date.firstDay), moment(date.lastDay))) {
@@ -145,10 +154,10 @@ const Rides = (props: IRidesProps) => {
 	};
 
 	const renderOwnerList = () => (
-		<RidesOwner firstDay={date.firstDay} lastDay={date.lastDay} rideSelected={selectedRide} setRide={setRide} rides={ridesOwner} />
+		<RidesOwner firstDay={date.firstDay} lastDay={date.lastDay} rideSelected={selectedRide} setRide={setRide} rides={props.ridesOwned} />
 	);
 	const renderParticipantList = () => (
-		<RidesParticipant firstDay={date.firstDay} lastDay={date.lastDay} rideSelected={selectedRide} setRide={setRide} rides={ridesParticipant}/>
+		<RidesParticipant firstDay={date.firstDay} lastDay={date.lastDay} rideSelected={selectedRide} setRide={setRide} rides={props.ridesParticipated} />
 	);
 
 	const UserSwitch = withStyles({
@@ -173,7 +182,7 @@ const Rides = (props: IRidesProps) => {
 		let list: JSX.Element;
 		if (userOwner) {
 			list = renderOwnerList();
-		}	else {
+		} else {
 			list = renderParticipantList();
 		}
 		return list;
@@ -183,7 +192,7 @@ const Rides = (props: IRidesProps) => {
 	const { t } = props;
 
 	return (
-		<div className = {cssClasses.container}>
+		<div className={cssClasses.container}>
 			<div className={cssClasses.leftPanel}>
 				<div className={cssClasses.leftLabels}>
 					<span> {t("Moje przejazdy")} </span>
@@ -191,18 +200,18 @@ const Rides = (props: IRidesProps) => {
 						style={ButtonLinkStyle.Button}
 						color={ButtonLinkColor.Gray}
 						background={ButtonLinkBackground.Gray}
-						// to={`${url}${GroupsRouter.routes.addGroup}`}
+					// to={`${url}${GroupsRouter.routes.addGroup}`}
 					>
 						{t(resources.add)}
 					</ButtonLink>
 				</div>
 				<div className={cssClasses.switch}>
 					<span className={switchCssClass.from} id={ids.from}> {t(resources.participant)}</span>
-						<FormControlLabel
-							control={<UserSwitch size="medium" checked={userOwner} onChange={handleSwitchChange} />}
-							label=""
-						/>
-					<span className={switchCssClass.to} id={ids.to}> {t(resources.owner) }</span>
+					<FormControlLabel
+						control={<UserSwitch size="medium" checked={userOwner} onChange={handleSwitchChange} />}
+						label=""
+					/>
+					<span className={switchCssClass.to} id={ids.to}> {t(resources.owner)}</span>
 				</div>
 				<div className={cssClasses.dateBar}>
 					<div>
@@ -212,7 +221,7 @@ const Rides = (props: IRidesProps) => {
 							background={ButtonSmallBackground.White}
 							icon={ButtonSmallIcon.Left}
 							onClick={() => onPrevDate()}
-						/ >
+						/>
 					</div>
 					<div className={cssClasses.dateBarRange}>{date.range}</div>
 					<div >
@@ -222,12 +231,12 @@ const Rides = (props: IRidesProps) => {
 							background={ButtonSmallBackground.White}
 							icon={ButtonSmallIcon.Right}
 							onClick={() => onNextDate()}
-						/ >
+						/>
 					</div>
 				</div>
 				<div className={cssClasses.leftOutline}></div>
 				<div className={cssClasses.leftList}>
-				{renderList()}
+					{renderList()}
 				</div>
 			</div>
 			<MediaQuery query="(min-width: 900px)">
@@ -239,4 +248,8 @@ const Rides = (props: IRidesProps) => {
 	);
 };
 
-export default withTranslation()(withRouter(Rides));
+export default withTranslation()(
+	withRouter(
+		connect(mapStateToProps, null)(Rides)
+	)
+);
