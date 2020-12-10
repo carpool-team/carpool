cacefd3b43e938096630708719fda7c3334d2fdd
Julian Kobryński <juliankobrynski@gmail.com>
2020-12-10 21:24:42 +0100

    CARP-517 Prevent app from crashing on Android

--- src/Mobile/carpool/src/components/Route/RouteMinimap.js
+++ src/Mobile/carpool/src/components/Route/RouteMinimap.js
@@ -1,13 +1,12 @@
 import React, {useState, useEffect} from 'react';
-import {View, ActivityIndicator, Text, StyleSheet} from 'react-native';
+import {View, ActivityIndicator, StyleSheet, Alert} from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
-import {multiPoint} from '@turf/helpers';
 import bbox from '@turf/bbox';
 import {activeRouteStyle, colors, sheet} from '../../styles';
-import {useGetDirections} from '../../hooks';
 import {BlueMarker} from '../common/map';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {MAP_LIGHT} from '@env';
+import {directionsClient} from '../../maps/mapbox';
 
 const dirConfig = {
   profile: 'driving',
@@ -16,14 +15,14 @@ const dirConfig = {
 };
 
 const getBounds = route => {
-  const boundingBox = bbox(multiPoint(route.geometry.coordinates));
+  const boundingBox = bbox(route.geometry);
   const [ne1, ne2, sw1, sw2] = boundingBox;
 
   return {
     paddingLeft: 32,
     paddingRight: 32,
-    paddingTop: 30,
-    paddingBottom: 30,
+    paddingTop: 32,
+    paddingBottom: 32,
     ne: [ne1, ne2],
     sw: [sw1, sw2],
   };
@@ -35,30 +34,41 @@ const parseCoords = coords => {
 
 const RouteMinimap = ({stops, hideDetails = false}) => {
   const [route, setRoute] = useState(null);
+
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   const [bounds, setBounds] = useState(null);
 
   useEffect(() => {
-    if (route) {
-      setBounds(getBounds(route));
-    }
-  }, [route]);
-
-  // Directions
-  const [results, loading, error, _getDirections] = useGetDirections({
-    ...dirConfig,
-  });
-
-  useEffect(() => {
     if (stops.length) {
-      _getDirections(stops.map(stop => parseCoords(stop.coordinates)));
+      const waypoints = stops.map(stop => ({
+        coordinates: parseCoords(stop.coordinates),
+      }));
+
+      setLoading(true);
+      directionsClient
+        .getDirections({
+          ...dirConfig,
+          waypoints,
+        })
+        .send()
+        .then(res => {
+          setResults(res);
+          setRoute(res.body.routes[0]);
+          setBounds(getBounds(res.body.routes[0]));
+        })
+        .catch(err => {
+          Alert.alert('Error!', 'An error occured', [
+            {
+              text: 'Cancel',
+              style: 'destructive',
+            },
+          ]);
+        })
+        .finally(() => setLoading(false));
     }
   }, [stops]);
-
-  useEffect(() => {
-    if (results) {
-      setRoute(results.body.routes[0]);
-    }
-  }, [results]);
 
   const renderPoints = () => {
     if (route) {
@@ -111,74 +121,20 @@ const RouteMinimap = ({stops, hideDetails = false}) => {
   const renderRoutes = () => {
     return route ? (
       <MapboxGL.ShapeSource id="routeShape" shape={route.geometry}>
-        <MapboxGL.LineLayer
-          id="routeLayer"
-          style={activeRouteStyle}
-          layerIndex={40}
-        />
+        <MapboxGL.LineLayer id="routeLayer" style={activeRouteStyle} />
       </MapboxGL.ShapeSource>
     ) : null;
   };
 
-  const renderTime = duration => {
-    const minutes = Math.round(duration / 60);
-    const hours = (minutes - (minutes % 60)) / 60;
-    const extraMinutes = minutes % 60;
-
-    return minutes > 60 ? (
-      <Text>
-        <Text style={styles.time}>{hours}</Text>
-        <Text style={styles.timeUnit}>h </Text>
-        <Text style={styles.time}>{extraMinutes}</Text>
-        <Text style={styles.timeUnit}>min</Text>
-      </Text>
-    ) : (
-      <Text>
-        <Text style={styles.time}>{minutes}</Text>
-        <Text style={styles.timeUnit}> min</Text>
-      </Text>
-    );
-  };
-
-  const renderDistance = distance => {
-    const kilometers = (distance / 1000).toFixed(0);
-    const meters = (distance / 1000).toFixed(1) * 1000;
-
-    return kilometers < 1 ? (
-      <Text>
-        <Text style={styles.distance}>{meters}</Text>
-        <Text style={styles.distanceUnit}> m</Text>
-      </Text>
-    ) : (
-      <Text>
-        <Text style={styles.distance}>{kilometers}</Text>
-        <Text style={styles.distanceUnit}> km</Text>
-      </Text>
-    );
-  };
-
-  const renderRouteDetails = () => {
-    if (route) {
-      const {distance, duration} = route;
-
-      return (
-        <View style={styles.routeDetailsWrapper}>
-          {renderDistance(distance)}
-          {renderTime(duration)}
-        </View>
-      );
-    } else {
-      return null;
-    }
-  };
-
-  return (
+  return loading || !route ? (
+    <View style={styles.centered}>
+      <ActivityIndicator color={colors.blue} size="large" />
+    </View>
+  ) : (
     <View style={styles.container}>
-      {!hideDetails && renderRouteDetails()}
       <MapboxGL.MapView
         style={{flex: 1}}
         styleURL={MAP_LIGHT}
-        // contentInset={10}
         compassEnabled={false}
         scrollEnabled={false}
         pitchEnabled={false}
@@ -186,17 +142,10 @@ const RouteMinimap = ({stops, hideDetails = false}) => {
         <MapboxGL.Camera
           maxZoomLevel={19}
           animationMode="flyTo"
-          animationDuration={0}
+          animationDuration={1000}
           bounds={bounds ? bounds : undefined}
         />
         {renderRoutes()}
-        {loading ? (
-          <ActivityIndicator
-            size="large"
-            color={colors.orange}
-            style={styles.activityIndicator}
-          />
-        ) : null}
         {renderPoints()}
       </MapboxGL.MapView>
     </View>
@@ -244,6 +193,11 @@ const styles = StyleSheet.create({
   },
   marker: {
     marginBottom: 32,
+  },
+  centered: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
