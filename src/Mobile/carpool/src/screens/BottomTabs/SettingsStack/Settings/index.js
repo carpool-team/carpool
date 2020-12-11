2142476692aa5d54616be52c03ecbf871bf627da
Julian Kobryński <juliankobrynski@gmail.com>
2020-12-11 19:03:00 +0100

    CARP-533 Logout user button

--- src/Mobile/carpool/src/screens/BottomTabs/SettingsStack/Settings/index.js
+++ src/Mobile/carpool/src/screens/BottomTabs/SettingsStack/Settings/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+import {StandardButton} from '../../../../components/common/buttons';
+import {colors} from '../../../../styles';
+import * as actions from '../../../../store/actions';
+import {useDispatch} from 'react-redux';
 
 const Settings = () => {
+  const dispatch = useDispatch();
+
+  const onLogout = () => dispatch(actions.logoutUser());
+
   return (
     <SafeAreaView style={styles.safeArea}>
-      <Text>TODO: Settings</Text>
+      <View style={styles.container}>
+        <Text>TODO: Settings</Text>
+        <StandardButton color={colors.red} onPress={onLogout} title="Logout" />
+      </View>
     </SafeAreaView>
   );
 };
@@ -13,6 +24,12 @@ const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
     width: '100%',
+  },
+  container: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-around',
   },
 });
 
