e94980ab32252736d7b819f0897562fce4d49895
Julian Kobryński <juliankobrynski@gmail.com>
2020-12-11 21:02:58 +0100

    CARP-536 New custom header component

---
+++ src/Mobile/carpool/src/components/navigation/Header/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import AccountSwitch from '../AccountSwitch';
+import {styles} from './index.styles';
+
+const Header = props => {
+  const {top} = useSafeAreaInsets();
+  const {headerLeft, title} = props.scene.descriptor.options;
+
+  return (
+    <View
+      style={{
+        marginTop: top,
+        ...styles.container,
+      }}>
+      <View style={styles.left}>{!!headerLeft && headerLeft()}</View>
+      <View style={styles.center}>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+      <View style={styles.right}>
+        <AccountSwitch />
+      </View>
+    </View>
+  );
+};
+
+export default Header;
---
+++ src/Mobile/carpool/src/components/navigation/Header/index.styles.js
@@ -0,0 +1,33 @@
+import {StyleSheet} from 'react-native';
+import {colors, sheet} from '../../../styles';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.background,
+    width: '100%',
+    height: 75,
+    borderBottomColor: colors.gray,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    ...sheet.rowCenterSplit,
+    paddingHorizontal: 16,
+  },
+  left: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  center: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  title: {
+    ...sheet.textMedium,
+    color: colors.grayVeryDark,
+    fontSize: 20,
+  },
+  right: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+});
