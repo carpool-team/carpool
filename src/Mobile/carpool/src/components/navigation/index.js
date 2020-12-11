ac2b7ff7411e83ce612cf320f74397acb1f286f6
Julian Kobryński <juliankobrynski@gmail.com>
2020-12-11 21:31:50 +0100

    CARP-536 Custom header go back button

--- src/Mobile/carpool/src/components/navigation/index.js
+++ src/Mobile/carpool/src/components/navigation/index.js
@@ -3,5 +3,13 @@ import CustomDrawer from './CustomDrawer';
 import HamburgerMenu from './HamburgerMenu';
 import CloseButton from './CloseButton';
 import Header from './Header';
+import GoBack from './GoBack';
 
-export {AccountSwitch, CustomDrawer, HamburgerMenu, CloseButton, Header};
+export {
+  AccountSwitch,
+  CustomDrawer,
+  HamburgerMenu,
+  CloseButton,
+  Header,
+  GoBack,
+};
