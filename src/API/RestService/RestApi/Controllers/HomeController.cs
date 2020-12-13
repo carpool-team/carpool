012707bfdd18e6f4f8f05678a73c414b97d15e32
Michał Dulski <dulski.michal@outlook.com>
2020-12-13 16:34:05 +0100

    feat: Added extension for reading AppUserId from jwt

--- src/API/RestService/RestApi/Controllers/HomeController.cs
+++ src/API/RestService/RestApi/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoWrapper.Wrappers;
+using Domain.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using RestApi.Extensions;
 
 namespace RestApi.Controllers
 {
@@ -16,6 +20,11 @@ namespace RestApi.Controllers
 
 		[Authorize("ApiScope")]
 		[HttpGet("claims")]
-		public async Task<ApiResponse> GetClaims() => new(from c in User.Claims select new {c.Type, c.Value});
+		public async Task<ApiResponse> GetClaims()
+		{
+			var appUserId = User.GetUserId();
+
+			return new(from c in User.Claims select new {c.Type, c.Value});
+		}	
 	}
 }
