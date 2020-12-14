37c5452cbb5bec3c5c913a558db7c923edefd618
Norbert Litkowski <norbert.litkowski@gmail.com>
2020-12-14 23:37:41 +0100

    CARP-574 Simplify adding group

--- src/Web/src/components/groups/components/addGroupForm/interfaces/IFormData.ts
+++ src/Web/src/components/groups/components/addGroupForm/interfaces/IFormData.ts
@@ -1,34 +1,7 @@
-import { IFormUserData } from "./IFormUserData";
 import { IFormGroupData } from "./IFormGroupData";
 
 /** Form data pending */
 export interface IFormData {
 	/** Pending group data */
 	group: IFormGroupData;
-	/** Pending user data */
-	user: IFormUserData;
-	/** Users added */
-	users: IFormUserData[];
-	/** Form's step */
-	step: number;
 }
-
-/** Initial data for use in form component */
-export const initialFormData: IFormData = {
-	group: {
-		address: "",
-		code: "",
-		groupName: "",
-		location: {
-			latitude: 0,
-			longitude: 0,
-		},
-	},
-	user: {
-		name: "",
-		surname: "",
-		email: "",
-	},
-	users: [],
-	step: 1,
-};
