35be1f153faead7bf7fc5a392e8ddb9f8de93556
Julian Kobryński <juliankobrynski@gmail.com>
2020-12-16 18:52:23 +0100

    CARP-583 Updated header in search stack

--- src/Mobile/carpool/src/navigation/MainStackNavigator/BottomTabs/SearchStack/index.js
+++ src/Mobile/carpool/src/navigation/MainStackNavigator/BottomTabs/SearchStack/index.js
@@ -1,22 +1,16 @@
 import * as React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {Search} from '../../../../screens/BottomTabs';
-import {sheet, colors} from '../../../../styles';
+import {Header} from '../../../../components/navigation';
 
 const Stack = createStackNavigator();
 
 const SearchStack = () => (
   <Stack.Navigator
     screenOptions={{
-      headerTitle: 'Search for a ride',
-      headerLeft: null,
-      headerTitleStyle: {
-        ...sheet.textMedium,
-        color: colors.grayVeryDark,
-      },
-      headerStyle: {
-        height: 110,
-      },
+      title: 'Find a ride',
+      header: props => <Header {...props} hideSwitch />,
+      gestureEnabled: false,
     }}>
     <Stack.Screen name="Search" component={Search} />
   </Stack.Navigator>
