b70e0b45113d6b8966144f6b8644adea03dcfa16
Maciej Sobkowiak <masobkowiak@gmail.com>
2020-12-18 02:05:25 +0100

    CARP-591 Send mails to bugtracker

--- src/Web/src/components/helpForm/HelpForm.tsx
+++ src/Web/src/components/helpForm/HelpForm.tsx
@@ -10,90 +10,112 @@ import { FormControl, InputLabel, MenuItem, Select } from "@material-ui/core";
 import Button from "../ui/button/Button";
 import { ButtonBackground } from "../ui/button/enums/ButtonBackground";
 import { ButtonColor } from "../ui/button/enums/ButtonColor";
+import * as emailjs from "emailjs-com";
+
 
 interface IHelpFormProps extends IReactI18nProps {
 }
 
-interface IUserAutocompleteData {
-	[email: string]: {
-		firstName: string;
-		lastName: string;
-		appUserId: string;
-	};
+interface IEmailData {
+	title: string;
+	body: string;
 }
 
 const resources = {
 	title: "helpForm.titleInput",
 	body: "helpForm.bodyInput",
 	send: "helpForm.send",
-	application: "helpForm.application"
+	application: "helpForm.application",
+	helpLabel: "helpForm.helpLabel"
 };
 
 const cssClasses = {
 	container: "helpForm",
-	input: "helpForm--input",
-	form: "helpForm--form",
-	combo: "helpForm--combo"
+	input: "helpForm__inputs--input",
+	form: "helpForm__inputs--form",
+	combo: "helpForm__inputs--combo",
+	inputs: "helpForm__inputs",
+	image: "helpForm__image",
 };
 const application = {
 	Web: "Web",
-	Mobile: "Mobile"
+	Mobile: "Mobile",
 }
+const emailUserID: string = process.env.EMAIL_USER_ID
+const emailServiceID: string = process.env.EMAIL_SERVICE_ID
+const webTemplateID: string = "template_x449sek"
+const mobileTemplateID: string = "template_pd6ylot"
+
 
 const HelpForm: (props: IHelpFormProps) => JSX.Element = props => {
 	const { t } = props;
 
 	const [title, setTitle] = useState<string>("");
 	const [body, setBody] = useState<string>("");
-	const [app, setApp] = useState<string>(null);
+	const [app, setApp] = useState<string>(application.Web);
 
 	const handleCombobox = (event: React.ChangeEvent<{ value: unknown }>) => {
-		console.log(event.target.value);
 		setApp(event.target.value as string);
 	}
-	const trySend = () => {
 
+	const templateParams = {
+		subject: title,
+		message: body
+	};
+
+	const trySend = () => {
+		if (app === application.Mobile) {
+			emailjs.send(emailServiceID, mobileTemplateID, templateParams, emailUserID)
+		} else {
+			emailjs.send(emailServiceID, webTemplateID, templateParams, emailUserID)
+		}
 	}
 
 	return (
 		<div className={cssClasses.container}>
-			<form className={cssClasses.form} noValidate autoComplete="off">
-				<FormControl className={cssClasses.input}>
-					<InputLabel id="application">{t(resources.application)}</InputLabel>
-					<Select
-						labelId="application"
-						className={cssClasses.combo}
-						value={app}
-						onChange={handleCombobox}
-					>
-						<MenuItem value={application.Web}>{application.Web}</MenuItem>
-						<MenuItem value={application.Mobile}>{application.Mobile}</MenuItem>
-					</Select>
-				</FormControl>
-				<TextField
-					label={t(resources.title)}
-					fullWidth
-					className={cssClasses.input}
-					rows={1}
-					value={title}
-					onChange={event => setTitle(event.target.value)}
-				/>
-				<TextField
-					label={t(resources.body)}
-					multiline
-					fullWidth
-					className={cssClasses.input}
-					rows={10}
-					value={body}
-					onChange={event => setBody(event.target.value)}
-				/>
-			</form>
-			<Button
-				background={ButtonBackground.Blue}
-				color={ButtonColor.White}
-				onClick={() => trySend()}>
-				{t(resources.send)}
-			</Button>
+			<div className={cssClasses.inputs}>
+				<span>{t(resources.helpLabel)}</span>
+				<form className={cssClasses.form} noValidate autoComplete="off">
+					<FormControl className={cssClasses.input}>
+						<InputLabel id="application">{t(resources.application)}</InputLabel>
+						<Select
+							labelId="application"
+							className={cssClasses.combo}
+							value={app}
+							onChange={handleCombobox}
+						>
+							<MenuItem value={application.Web}>{application.Web}</MenuItem>
+							<MenuItem value={application.Mobile}>{application.Mobile}</MenuItem>
+						</Select>
+					</FormControl>
+					<TextField
+						label={t(resources.title)}
+						fullWidth
+						className={cssClasses.input}
+						rows={1}
+						value={title}
+						onChange={event => setTitle(event.target.value)}
+					/>
+					<TextField
+						label={t(resources.body)}
+						multiline
+						fullWidth
+						className={cssClasses.input}
+						rows={8}
+						value={body}
+						onChange={event => setBody(event.target.value)}
+					/>
+				</form>
+				<Button
+					background={ButtonBackground.Blue}
+					color={ButtonColor.White}
+					onClick={() => trySend()}>
+					{t(resources.send)}
+				</Button>
+			</div>
+			<div className={cssClasses.image}>
+			</div>
+
 		</div >
 	);
 };
