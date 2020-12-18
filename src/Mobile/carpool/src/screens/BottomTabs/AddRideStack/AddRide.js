3de3de0c14b207af8120961125e78f9e60c64e94
Julian Kobryński <juliankobrynski@gmail.com>
2020-12-18 19:23:47 +0100

    Add ride go back actions

--- src/Mobile/carpool/src/screens/BottomTabs/AddRideStack/AddRide.js
+++ src/Mobile/carpool/src/screens/BottomTabs/AddRideStack/AddRide.js
@@ -1,4 +1,4 @@
-import React, {useReducer} from 'react';
+import React, {useReducer, useEffect} from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 import {reducer, initialState, AddRideActions} from './reducer';
 import {
@@ -11,9 +11,52 @@ import {
   RideCreated,
   RideError,
 } from './sections';
+import {styles} from './index.styles';
+import {GoBack} from '../../../components/navigation';
+import {useIsFocused} from '@react-navigation/native';
 
 const AddRide = ({navigation}) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if (state.group) {
+      const onPress = () => {
+        if (state.seats !== null) {
+          dispatch({type: AddRideActions.SET_SEATS, payload: null});
+          return;
+        }
+        if (state.regular !== null) {
+          dispatch({type: AddRideActions.SET_REGULAR, payload: null});
+          return;
+        }
+        if (state.location) {
+          dispatch({type: AddRideActions.SET_LOCATION, payload: null});
+          return;
+        }
+        if (state.swap !== null) {
+          dispatch({type: AddRideActions.SET_SWAP, payload: null});
+          return;
+        }
+        if (state.group) {
+          dispatch({type: AddRideActions.SET_GROUP, payload: null});
+          return;
+        }
+      };
+
+      navigation.setOptions({
+        headerLeft: () => <GoBack onPress={onPress} />,
+      });
+    } else {
+      navigation.setOptions({
+        headerLeft: undefined,
+      });
+    }
+  }, [state]);
+
+  useEffect(() => {
+    !isFocused && dispatch({type: AddRideActions.CLEAN_STATE});
+  }, [isFocused]);
 
   const renderSection = () => {
     if (!state.group) {
@@ -49,12 +92,5 @@ const AddRide = ({navigation}) => {
 
   return <SafeAreaView style={styles.safeArea}>{renderSection()}</SafeAreaView>;
 };
-
-const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    width: '100%',
-  },
-});
 
 export default AddRide;
