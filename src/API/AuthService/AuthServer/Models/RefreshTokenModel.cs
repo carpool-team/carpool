98dd142936062999fc84c0fbfe2345a35e8ac227
Michał Dulski <dulski.michal@outlook.com>
2020-12-22 23:18:17 +0100

    CARP-616 improvement: Better refresh token model for refreshing token

---
+++ src/API/AuthService/AuthServer/Models/RefreshTokenModel.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace AuthServer.Models
+{
+	public class RefreshTokenModel
+	{
+		[JsonConstructor]
+		public RefreshTokenModel(string value)
+		{
+			Value = value;
+		}
+
+		public string Value { get; }
+	}
+}
