e50831a5f35daf80ab8673a5c86c7a177a4dff9d
Michał Dulski <dulski.michal@outlook.com>
2020-12-28 01:34:43 +0100

    fix: removing recurring rides

--- src/API/RestService/DataAccessLayer/Migrations/20201228002847_changed_recurring_ride_OnDelete_to_cascade.Designer.cs
+++ src/API/RestService/DataAccessLayer/Migrations/20201228002847_changed_recurring_ride_OnDelete_to_cascade.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.DatabaseContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(CarpoolDbContext))]
-    partial class CarpoolDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201228002847_changed_recurring_ride_OnDelete_to_cascade")]
+    partial class changed_recurring_ride_OnDelete_to_cascade
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/API/RestService/DataAccessLayer/Migrations/20201228002847_changed_recurring_ride_OnDelete_to_cascade.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class changed_recurring_ride_OnDelete_to_cascade : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rides_RecurringRides_RecurringRideId",
+                table: "Rides");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rides_RecurringRides_RecurringRideId",
+                table: "Rides",
+                column: "RecurringRideId",
+                principalTable: "RecurringRides",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Rides_RecurringRides_RecurringRideId",
+                table: "Rides");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Rides_RecurringRides_RecurringRideId",
+                table: "Rides",
+                column: "RecurringRideId",
+                principalTable: "RecurringRides",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
