a7087863ae37ab575ce4d4c5b1a4624c1656dfc2
nlitkowski <norbert.litkowski@gmail.com>
2021-01-25 22:55:47 +0100

    CARP-722 Add schedule loader

--- src/Web/src/components/shared/ridesList/components/lists/RidesListSchedule.tsx
+++ src/Web/src/components/shared/ridesList/components/lists/RidesListSchedule.tsx
@@ -12,6 +12,8 @@ import { IReactI18nProps } from "../../../../system/resources/IReactI18nProps";
 import { ILeaveRideAction, IDeleteRideAction, IDeletePassengerAction } from "../../../../rides/store/Types";
 import { leaveRide, deleteRide, deletePassenger } from "../../../../rides/store/Actions";
 import { connect } from "react-redux";
+import { LoadingStatus } from "../../../enum/LoadingStatus";
+import LoaderBlock from "../../../../ui/loaderBlock/LoaderBlock";
 
 interface IDispatchPropsType {
 	leaveRide: (rideId: string) => ILeaveRideAction;
@@ -120,11 +122,26 @@ const RidesListSchedule = (props: IRidesListScheduleProps) => {
 
 	let colorIndex: number = 0;
 	const rides: IRide[] = props.rides;
-	const days = [];
+	const days: string[] = [];
 
 	for (let m = moment(props.firstDay); m.diff(props.lastDay, "days") <= 0; m.add(1, "days")) {
 		days.push(m.format());
 	}
+
+	const renderItems = (day: string) => {
+		switch (props.loadingStatus) {
+			case LoadingStatus.Loading:
+				return <LoaderBlock />;
+			default:
+				if (rides) {
+					return rides.map((ride) => {
+						++colorIndex;
+						const color = colorList[colorIndex % colorList.length];
+						return renderItem(color, ride, day);
+					});
+				}
+		}
+	};
 
 	return (
 		<ul className={cssClasses.listContainer}>
@@ -133,14 +150,7 @@ const RidesListSchedule = (props: IRidesListScheduleProps) => {
 					<div className={cssClasses.day} key={day}>
 						<div className={cssClasses.dayLabel}>{moment(day).format("DD.MM")}</div>
 						<ul className={cssClasses.list}>
-							{rides && rides.map((ride) => {
-								++colorIndex;
-								const color = colorList[colorIndex % colorList.length];
-								return (
-									renderItem(color, ride, day)
-								);
-							}
-							)}
+							{renderItems(day)}
 						</ul>
 					</div>
 				);
